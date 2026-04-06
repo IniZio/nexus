89634d9306ef2900b531a283a364a875e3a2b007
Newman Chow <newman@oursky.com>
2026-04-06 10:59:38 +0800

    fix(nexus): restore non-darwin runtime bootstrap compile

--- packages/nexus/cmd/nexus/init_runtime_setup_other.go
+++ packages/nexus/cmd/nexus/init_runtime_setup_other.go
@@ -13,9 +13,13 @@ var initRuntimeBootstrapRunner func(projectRoot, runtimeName string) error = run
 var (
 	initRuntimeBootstrapIsRootFn                   = func() bool { return false }
 	initRuntimeBootstrapSudoOKFn                   = func() bool { return false }
-	initRuntimeBootstrapIsTTYFn                    = isTerminal
+	initRuntimeBootstrapIsTTYFn                    = isTerminalUnsupported
 	initRuntimeBootstrapSkipFastFailFn func() bool = nil
 )
+
+func isTerminalUnsupported(_ *os.File) bool {
+	return false
+}
 
 func runInitRuntimeBootstrapOther(projectRoot, runtimeName string) error {
 	if runtimeName != "firecracker" {
